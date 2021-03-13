df6d8135dd13c7781262020cbf07a7fa5c749259
MBtsky <9250499222@mail.ru>
2021-03-13 15:27:11 +0300

    invites in progress

--- src/components/tools/use-invites-fetcher.ts
+++ src/components/tools/use-invites-fetcher.ts
@@ -1,19 +1,20 @@
 import {useEffect} from 'react'
 import {useAppState} from './use-app-state'
+import {getTeam, personalInvites, teamInvites} from '../../model/api'
 
 
 export const useInvitesFetcher = () => {
 
-    const {cUser, cEvent} = useAppState()
+    const {cUser, cEvent, invites} = useAppState()
 
     useEffect(() => {
         (async () => {
-            // const [team, personal] = await Promise.all([teamInvites(cEvent.id, cUser.id), personalInvites(cEvent.id, cUser.id)])
-            // invites.set({team, personal})
-            // const t = team.map(u => getTeam(cEvent.id, u.id))
-            // const teams = await Promise.all(t)
-            // teams.forEach((t, i) => {team[i].team = t})
-            // invites.set({team, personal})
+            const [team, personal] = await Promise.all([teamInvites(cEvent.id, cUser.id), personalInvites(cEvent.id, cUser.id)])
+            invites.set({team, personal})
+            const t = team.map(u => getTeam(cEvent.id, u.id))
+            const teams = await Promise.all(t)
+            teams.forEach((t, i) => {team[i].team = t})
+            invites.set({team, personal})
         })()
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [cUser.id, cEvent.id])
