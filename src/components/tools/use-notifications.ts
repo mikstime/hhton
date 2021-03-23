600954883d53e21b32981fffbbb52ba81690407f
MBtsky <9250499222@mail.ru>
2021-03-23 14:30:10 +0300

    ws increment

--- src/components/tools/use-notifications.ts
+++ src/components/tools/use-notifications.ts
@@ -1,24 +1,35 @@
 import {useAppState} from './use-app-state'
 import {useEffect, useRef} from 'react'
-import { w3cwebsocket} from 'websocket'
-import {PREFIX, WS_DOMAIN} from '../../config/network'
+import {w3cwebsocket} from 'websocket'
+import {WS_DOMAIN} from '../../config/network'
 import {useSnackbar} from 'notistack'
 
 export const useNotifications = () => {
     const {cUser} = useAppState()
 
-    const client = useRef<null|w3cwebsocket>(null)
+    const client = useRef<null | w3cwebsocket>(null)
     const {enqueueSnackbar} = useSnackbar()
     useEffect(() => {
-        if(cUser.id !== '-1') {
-            if(client.current) client.current.close()
-
-            client.current = new w3cwebsocket(`${WS_DOMAIN}${PREFIX}/user/${cUser.id}/connect`)
-            client.current.onopen = () => {
-                console.log('connection established')
-            }
-            client.current.onmessage = (m) => {
-                enqueueSnackbar(m)
+        if (cUser.id !== '-1') {
+            if (client.current) client.current.close()
+            try {
+                client.current = new w3cwebsocket(`${WS_DOMAIN}:8080/notification/channel/${cUser.id}`)
+                client.current.onmessage = (m) => {
+                    console.log(m)
+                    enqueueSnackbar(m.data)
+                }
+                client.current.onopen = () => {
+                    console.log('connection established')
+                    if (client.current) {
+                        // console.log('sending')
+                        client.current.send('hello')
+                    }
+                }
+                client.current.onerror = (e) => {
+                    console.log(e)
+                }
+            } catch (e) {
+                console.log(e)
             }
         }
     }, [cUser.id])
