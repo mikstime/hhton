f2fb1b71523cb9e28e24716443d0a5343f35b5e8
Huvalk <akiraq@yandex.ru>
2021-04-03 13:44:33 +0300

    feat(Api): change team name, invites history

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,4 +1,4 @@
-const local = false
+const local = true
 
 export const WS_DOMAIN = local ? 'ws://localhost:8080' : 'wss://team-up.online'
 export const HOST_DOMAIN = local ? 'http://localhost:8080' : 'https://team-up.online'
