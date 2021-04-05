6185d36635bdf13e37e1d888746595d8601a98b3
Huvalk <akiraq@yandex.ru>
2021-04-05 14:31:16 +0300

    fix: network url

--- src/config/network.ts
+++ src/config/network.ts
@@ -3,6 +3,6 @@ const env = 'dev'
 // @ts-ignore
 export const WS_DOMAIN = env === 'local' ? 'ws://localhost:8080' : env === 'prod' ? 'wss://team-up.online' : 'wss://dev.team-up.online'
 // @ts-ignore
-export const HOST_DOMAIN = env === 'local' ? 'http://localhost:8080' : env === 'prod' ? 'https://team-up.online' : 'wss://dev.team-up.online'
+export const HOST_DOMAIN = env === 'local' ? 'http://localhost:8080' : env === 'prod' ? 'https://team-up.online' : 'https://dev.team-up.online'
 // @ts-ignore
 export const PREFIX = env === 'local' ? '' : '/api'
