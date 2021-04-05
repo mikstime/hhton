ebdf499f24e13db99a168c707760998897fd9def
Huvalk <akiraq@yandex.ru>
2021-04-05 19:16:21 +0300

    fix: deploy

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,4 +1,4 @@
-const env = 'dev'
+const env = process.env.REACT_APP_ENV
 
 // @ts-ignore
 export const WS_DOMAIN = env === 'local' ? 'ws://localhost:8080' : env === 'prod' ? 'wss://team-up.online' : 'wss://dev.team-up.online'
