4feab0600b60513e04b9c5902dec9a0b8978a80e
MBtsky <9250499222@mail.ru>
2021-04-13 01:31:30 +0300

    Revert "attempt 2"
    This reverts commit 9472b9a4

--- src/components/tools/use-fetcher.ts
+++ src/components/tools/use-fetcher.ts
@@ -23,11 +23,6 @@ export const useFetcher = () => {
     const appState = useAppState()
 
     useEffect(() => {
-        isFetchingUserId.current = '-1'
-        isFetchingCuserId.current = '-1'
-    }, [nc.updates])
-
-    useEffect(() => {
         (async () => {
             if (appState.user.id !== isFetchingUserId.current && appState.user.id !== '-1') {
                 isFetchingUserId.current = appState.user.id
