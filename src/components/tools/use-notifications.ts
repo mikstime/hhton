5ed6eda0f6cce34afc1d29af5a74fd6d220f1157
MBtsky <9250499222@mail.ru>
2021-05-03 17:29:14 +0300

    better socket

--- src/components/tools/use-notifications.ts
+++ src/components/tools/use-notifications.ts
@@ -9,36 +9,41 @@ export const useNotifications = () => {
     const {cUser} = useAppState()
     const nc = useNotificationHandlers()
     const client = useRef<null | w3cwebsocket>(null)
-    const shouldReconnect = useRef(true)
+    const shouldReconnect = useRef(false)
+
     useEffect(() => {
         if (cUser.id !== '-1' && !cUser.isNullUser) {
-            if (client.current) {
-                shouldReconnect.current = false
-                client.current.close()
-                setTimeout(() => {
-                    shouldReconnect.current = true
-                }, 1000)
-            }
-            client.current = new w3cwebsocket(
-                `${WS_DOMAIN}${PREFIX}/notification/channel/${cUser.id}`
-            )
+            shouldReconnect.current = true
         }
     }, [cUser.id, cUser.isNullUser])
-    useEffect(() => {
-        if (!client.current) return
 
-        client.current.onmessage = (m) => {
-            const json = JSON.parse(m.data as string) as Message
-            nc[json.status] ? nc[json.status](json) : nc.default(json)
-        }
-        client.current.onerror = () => {
-        }
-        client.current.onclose = () => {
-            if(shouldReconnect.current) {
+    useEffect(() => {
+        const id = setInterval(() => {
+            if (shouldReconnect.current) {
+                if (client.current) {
+                    client.current.close()
+                }
                 client.current = new w3cwebsocket(
                     `${WS_DOMAIN}${PREFIX}/notification/channel/${cUser.id}`
                 )
+                client.current.onopen = () => {
+                    shouldReconnect.current = false
+                }
+                client.current.onmessage = (m) => {
+                    const json = JSON.parse(m.data as string) as Message
+                    nc[json.status] ? nc[json.status](json) : nc.default(json)
+                }
+                client.current.onerror = () => {
+                    shouldReconnect.current = true
+                }
+                client.current.onclose = () => {
+                    shouldReconnect.current = true
+                }
             }
+        }, 1000)
+
+        return () => {
+            clearInterval(id)
         }
-    }, [client.current, nc, shouldReconnect.current])
+    }, [cUser.id])
 }
